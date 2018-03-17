705733bca102784d1ce2cda959bf5b4ede5870b0
John Cannon <john.cannon@sydney.edu.au>
2018-03-17 12:19:52 +1100

    Avoid "Read the Docs" warning about html_static_path not existing.

--- docs/conf.py
+++ docs/conf.py
@@ -162,7 +162,13 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#
+# Setting empty path to avoid the following warning in Read the Doc doc builds...
+#
+#   WARNING: html_static_path entry '/home/docs/checkouts/readthedocs.org/readthedocs/templates/sphinx/_static' does not exist
+#
+# html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
