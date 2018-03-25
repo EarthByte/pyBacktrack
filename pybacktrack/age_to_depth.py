8c1c28a02fa71074dfd5e498442fd2c5d8df6b65
John Cannon <john.cannon@sydney.edu.au>
2018-03-25 14:26:49 +1100

    Replace stdin/stdout with files and use numpydoc in docstrings.

--- pybacktrack/age_to_depth.py
+++ pybacktrack/age_to_depth.py
@@ -1,25 +1,27 @@
 
-"""
-    Copyright (C) 2017 The University of Sydney, Australia
-    
-    This program is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License, version 2, as published by
-    the Free Software Foundation.
-    
-    This program is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-    
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-"""
+#
+# Copyright (C) 2017 The University of Sydney, Australia
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License, version 2, as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
+"""Convert ocean basin ages (Ma) to basement depth (metres) using different age/depth models.
 
-###############################################################################
-# Various age/depth curves to convert ocean basin age (Ma) to depth (metres). #
-###############################################################################
+:func:`age_to_depth` converts ocean basin age to basement depth using a specified age/depth model.
+
+:func:`age_to_depth_file` converts age to depth by reading `lon, lat, age` rows from input file and writing `lon, lat, depth` rows to output file.
+"""
 
 
 from __future__ import print_function
@@ -53,14 +55,27 @@ DEFAULT_MODEL = MODEL_GDH1
 def age_to_depth(
         age,
         model=DEFAULT_MODEL):
-    """
-    Converts 'age' (Ma) to basement depth (metres) using the age/depth curve/model 'model'.
+    """Convert ocean basin age to basement depth using a specified age/depth model.
     
-    Returns depth (in metres) as a positive number.
+    Parameters
+    ----------
+    age : float
+        The age in Ma.
+    model : {MODEL_GDH1, MODEL_CROSBY_2007}, optional
+        The model to use when converting ocean age to basement depth.
+        Defaults to `MODEL_GDH1` (Stein and Stein 1992).
     
-    'model' defaults to GDH1 (Stein and Stein 1992).
+    Returns
+    -------
+    float
+        Depth (in metres) as a positive number.
     
-    'age' must be non-negative.
+    Raises
+    ------
+    ValueError
+        If `age` is negative.
+    ValueError
+        If `model` is not a recognised model.
     """
     
     if age < 0:
@@ -74,42 +89,59 @@ def age_to_depth(
     raise ValueError('Unexpected model.')
 
 
-def age_to_depth_from_stdin_to_stdout(
+def age_to_depth_file(
+        input_filename,
+        output_filename,
         model=DEFAULT_MODEL):
-    """
-    Converts (lon, lat, age) lines on stdin to (lon, lat, depth) on stdout.
+    """Converts age to depth by reading `lon, lat, age` rows from input file and writing `lon, lat, depth` rows to output file.
     
-    Reads text data from standard input where each line contains longitude, latitude (in degrees) and age (in Ma).
-    Writes text data to standard output where each line contains longitude, latitude (in degrees) and depth (in metres).
+    Parameters
+    ----------
+    input_filename : string
+        Name of input text file containing the `lon, lat, age` rows.
+        Age is in Ma.
+    output_filename : string
+        Name of output text file containing `lon, lat, depth` rows.
+        Depth is in metres.
+        Each `depth` in output row is converted from `age` in associated input row.
+    model : {MODEL_GDH1, MODEL_CROSBY_2007}, optional
+        The model to use when converting ocean age to basement depth.
+    
+    Raises
+    ------
+    ValueError
+        If cannot read `lon, lat, age` values, as floating-point numbers, from input file.
     """
     
-    for line_number, line in enumerate(sys.stdin):
+    with open(input_filename, 'rU') as input_file, open(output_filename, 'w') as output_file:
+        for line_number, line in enumerate(input_file):
 
-        # Make line number 1-based instead of 0-based.
-        line_number = line_number + 1
+            # Make line number 1-based instead of 0-based.
+            line_number = line_number + 1
 
-        # Split the line into strings (separated by whitespace).
-        line_string_list = line.split()
+            # Split the line into strings (separated by whitespace).
+            line_string_list = line.split()
 
-        # Need at least 3 strings per line (longitude, latitude and age).
-        if len(line_string_list) < 3:
-            print('Line {0}: Ignoring point - line does not have at least three white-space separated strings.'.format(
-                  line_number), file=sys.stderr)
-            continue
+            # Need at least 3 strings per line (longitude, latitude and age).
+            if len(line_string_list) < 3:
+                print('Line {0} in {1}: Ignoring point - line does not have at least three white-space separated strings.'.format(
+                      line_number, input_filename), file=sys.stderr)
+                continue
 
-        # Attempt to convert each string into a floating-point number.
-        try:
-            # Use GMT (lon/lat) order.
-            lon = float(line_string_list[0])
-            lat = float(line_string_list[1])
-            age = float(line_string_list[2])
-        except ValueError:
-            print('Line {0}: Ignoring point - cannot read lon/lat/age values.'.format(line_number), file=sys.stderr)
-            continue
-        
-        depth = age_to_depth(age, model)
-        
-        sys.stdout.write('{0}\t{1}\t{2}\n'.format(lon, lat, depth))
+            # Attempt to convert each string into a floating-point number.
+            try:
+                # Use GMT (lon/lat) order.
+                lon = float(line_string_list[0])
+                lat = float(line_string_list[1])
+                age = float(line_string_list[2])
+            except ValueError:
+                print('Line {0} in {1}: Ignoring point - cannot read lon/lat/age values.'.format(
+                      line_number, input_filename), file=sys.stderr)
+                continue
+            
+            depth = age_to_depth(age, model)
+            
+            output_file.write('{0}\t{1}\t{2}\n'.format(lon, lat, depth))
 
 
 #########################################################################################
@@ -134,24 +166,29 @@ def age_to_depth_GDH1(age):
 # Converted to Python from C program "age2depth.c".                                     #
 #########################################################################################
 
-CROSBY_2007_DENSM = 3300.0  # Mantle density, kgm-3
-CROSBY_2007_DENSW = 1030.0  # Water density, kgm-3
-CROSBY_2007_KAPPA = 7.8e-7  # Thermal diffusivity, m2s-1
-CROSBY_2007_ALPHA = 3.2e-5  # Thermal expansivity
-CROSBY_2007_TM = 1333.0  # Mantle temperature, C
-CROSBY_2007_RD = 2600.0  # Zero-age depth, m
-CROSBY_2007_PTHICK = 1.02e5  # Plate thickness, m
-
-CROSBY_2007_PERT_A = 300.0
-CROSBY_2007_PERT_B = 15.0
-CROSBY_2007_PERT_C = 0.2
-CROSBY_2007_PERT_D = 94.0
-CROSBY_2007_PERT_E = 30.0
-
-CROSBY_2007_FTOL = 1.0e-6
+def age_to_depth_CROSBY_2007(age):
+    
+    return _CROSBY_2007_RD + _CROSBY_2007_subs(age) - _CROSBY_2007_pert(age)
 
 
-def CROSBY_2007_subs(age):
+_CROSBY_2007_DENSM = 3300.0  # Mantle density, kgm-3
+_CROSBY_2007_DENSW = 1030.0  # Water density, kgm-3
+_CROSBY_2007_KAPPA = 7.8e-7  # Thermal diffusivity, m2s-1
+_CROSBY_2007_ALPHA = 3.2e-5  # Thermal expansivity
+_CROSBY_2007_TM = 1333.0  # Mantle temperature, C
+_CROSBY_2007_RD = 2600.0  # Zero-age depth, m
+_CROSBY_2007_PTHICK = 1.02e5  # Plate thickness, m
+
+_CROSBY_2007_PERT_A = 300.0
+_CROSBY_2007_PERT_B = 15.0
+_CROSBY_2007_PERT_C = 0.2
+_CROSBY_2007_PERT_D = 94.0
+_CROSBY_2007_PERT_E = 30.0
+
+_CROSBY_2007_FTOL = 1.0e-6
+
+
+def _CROSBY_2007_subs(age):
 
     # Calculates the expected subsidence for a given age and plate model
 
@@ -162,7 +199,7 @@ def CROSBY_2007_subs(age):
     
     while True:
 
-        sum = math.exp(-i * i * math.pi * math.pi * CROSBY_2007_KAPPA * age / (CROSBY_2007_PTHICK * CROSBY_2007_PTHICK))
+        sum = math.exp(-i * i * math.pi * math.pi * _CROSBY_2007_KAPPA * age / (_CROSBY_2007_PTHICK * _CROSBY_2007_PTHICK))
         sum /= (i * i)
         sum *= -2.0
         sum += oldsum
@@ -173,35 +210,39 @@ def CROSBY_2007_subs(age):
         
         i += 2.0
     
-        if fdiff <= CROSBY_2007_FTOL:
+        if fdiff <= _CROSBY_2007_FTOL:
             break
 
-    w = sum * 2.0 * CROSBY_2007_TM * CROSBY_2007_PTHICK / (math.pi * math.pi)
-    w += CROSBY_2007_TM * CROSBY_2007_PTHICK / 2.0
-    w *= CROSBY_2007_DENSM * CROSBY_2007_ALPHA / (CROSBY_2007_DENSM - CROSBY_2007_DENSW)
+    w = sum * 2.0 * _CROSBY_2007_TM * _CROSBY_2007_PTHICK / (math.pi * math.pi)
+    w += _CROSBY_2007_TM * _CROSBY_2007_PTHICK / 2.0
+    w *= _CROSBY_2007_DENSM * _CROSBY_2007_ALPHA / (_CROSBY_2007_DENSM - _CROSBY_2007_DENSW)
 
     return w
 
 
-def CROSBY_2007_pert(age):
+def _CROSBY_2007_pert(age):
 
-    ptb = (age - CROSBY_2007_PERT_D) / CROSBY_2007_PERT_E
+    ptb = (age - _CROSBY_2007_PERT_D) / _CROSBY_2007_PERT_E
     ptb *= ptb
     ptb = math.exp(-ptb)
-    ptb *= math.sin((age / CROSBY_2007_PERT_B) - CROSBY_2007_PERT_C)
-    ptb *= CROSBY_2007_PERT_A
+    ptb *= math.sin((age / _CROSBY_2007_PERT_B) - _CROSBY_2007_PERT_C)
+    ptb *= _CROSBY_2007_PERT_A
 
     return ptb
-
-
-def age_to_depth_CROSBY_2007(age):
-    
-    return CROSBY_2007_RD + CROSBY_2007_subs(age) - CROSBY_2007_pert(age)
 
 
 if __name__ == '__main__':
     
     import argparse
+    
+    def argparse_unicode(value_string):
+        try:
+            # Filename uses the system encoding - decode from 'str' to 'unicode'.
+            filename = value_string.decode(sys.getfilesystemencoding())
+        except UnicodeDecodeError:
+            raise argparse.ArgumentTypeError("Unable to convert filename %s to unicode" % value_string)
+        
+        return filename
     
     model_dict = dict((model_name, model) for model, model_name, _ in ALL_MODELS)
     model_name_dict = dict((model, model_name) for model, model_name, _ in ALL_MODELS)
@@ -210,8 +251,8 @@ if __name__ == '__main__':
     __description__ = \
         """Converts (lon, lat, age) lines on stdin to (lon, lat, depth) on stdout.
         
-        Reads text data from standard input where each line contains longitude, latitude (in degrees) and age (in Ma).
-        Writes text data to standard output where each line contains longitude, latitude (in degrees) and depth (in metres).
+        Reads text data from input file where each line contains longitude, latitude (in degrees) and age (in Ma).
+        Writes text data to output file where each line contains longitude, latitude (in degrees) and depth (in metres).
         
         The age-to-depth model can be chosen using the '-m' option.
         Choices include:{0}
@@ -221,7 +262,7 @@ if __name__ == '__main__':
         NOTE: Separate the positional and optional arguments with '--' (workaround for bug in argparse module).
         For example...
 
-        python %(prog)s -m {1} < age_points.xy > depth_points.xy
+        python %(prog)s -m {1} age_points.xy depth_points.xy
         """.format(
             ''.join('\n\n        {0}: {1}.\n'.format(model_name, model_desc)
                     for _, model_name, model_desc in ALL_MODELS),
@@ -242,6 +283,16 @@ if __name__ == '__main__':
                 ', '.join(model_name for _, model_name, _ in ALL_MODELS),
                 default_model_name))
     
+    parser.add_argument(
+        'input_filename', type=argparse_unicode,
+        metavar='input_filename',
+        help='The input filename containing the "age" values.')
+    
+    parser.add_argument(
+        'output_filename', type=argparse_unicode,
+        metavar='output_filename',
+        help='The output filename containing the converted "depth" values.')
+    
     # Parse command-line options.
     args = parser.parse_args()
     
@@ -251,6 +302,9 @@ if __name__ == '__main__':
     except KeyError:
         raise argparse.ArgumentTypeError("%s is not a valid model" % args.model_name)
     
-    age_to_depth_from_stdin_to_stdout(model)
+    age_to_depth_file(
+        args.input_filename,
+        args.output_filename,
+        model)
     
     sys.exit(0)
