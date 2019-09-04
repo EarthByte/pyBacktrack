706eb720cee05ee5792bd57d822e312445c9d40e
John Cannon <john.cannon@sydney.edu.au>
2019-09-04 21:39:57 +1000

    Convert well formats like sunrise to pybacktrack format.
    These are ".txt", which have slightly different format to the ".dat" files already handled.

--- misc/convert_old_wells.py
+++ misc/convert_old_wells.py
@@ -134,8 +134,7 @@ def convert_dat_file(
         well_locations,
         lithologies):
     """
-    Reads the ".dat" filename and converts to pyBacktrack format
-    (with '_pybacktrack' suffixed to the file's basename).
+    Reads the ".dat" filename and converts to pyBacktrack format (in sub-directory 'pybacktrack').
     
     Parameters
     ----------
@@ -158,7 +157,7 @@ def convert_dat_file(
     
     # Skip comments.
     line_index = 0
-    while lines[line_index][0] == 'C':
+    while lines[line_index].split()[0] == 'C':
         line_index += 1
     
     # Read some header data.
@@ -169,18 +168,15 @@ def convert_dat_file(
     
     # Skip more comments.
     line_index += 6
-    while lines[line_index][0] == 'C':
+    while lines[line_index].split()[0] == 'C':
         line_index += 1
     
     # Find well location.
-    if well_name in well_locations:
-        well_latitude, well_longitude = well_locations[well_name]
-    else:
-        well_latitude, well_longitude = None, None
-        warnings.warn('Not writing well location for well file {0}. Cannot find a well location with well name "{1}".'.format(
+    if well_name not in well_locations:
+        warnings.warn('Skipping well file {0}. Cannot find a well location with well name "{1}".'.format(
             well_input_filename, well_name))
-    well.latitude = well_latitude
-    well.longitude = well_longitude
+        return
+    well.latitude, well.longitude = well_locations[well_name]
     
     # Read the stratigraphic layers.
     top_age = surface_age
@@ -198,14 +194,14 @@ def convert_dat_file(
         # Add the 8 hardwired lithology components.
         total_lithology_fraction = 0.0
         lithology_components = []
-        for lithology_component_index in range(0, 8):
-            lithology_fraction_column_index = 20 + 5 * lithology_component_index
+        for lithology_index in range(0, 8):
+            lithology_fraction_column_index = 20 + 5 * lithology_index
             lithology_fraction_string = layer_line[lithology_fraction_column_index:lithology_fraction_column_index+5].strip()
             # Add lithology component unless it's just whitespace.
             if lithology_fraction_string:
                 lithology_fraction = float(lithology_fraction_string)
                 total_lithology_fraction += lithology_fraction
-                lithology_components.append((DAT_LITHOLOGY_NAMES[lithology_component_index], lithology_fraction))
+                lithology_components.append((DAT_LITHOLOGY_NAMES[lithology_index], lithology_fraction))
         
         # Ensure the lithology fractions add up to 1.0.
         for lithology_component_index in range(len(lithology_components)):
@@ -250,11 +246,182 @@ def convert_dat_file(
         well_attributes={'longitude': 'SiteLongitude', 'latitude': 'SiteLatitude'})
 
 
+# The ".txt" format hardwires 8 lithologies in this order.
+TXT_LITHOLOGY_NAMES = [
+    'Shale',
+    'Limestone',
+    'Reef',
+    'Dolostone',
+    'Sand',
+    'Basalt',
+    'Anhydrite',
+    'Salt']
+
+
+def convert_txt_file(
+        well_input_filename,
+        well_locations,
+        lithologies):
+    """
+    Reads the ".txt" filename and converts to pyBacktrack format (in sub-directory 'pybacktrack').
+    
+    Parameters
+    ----------
+    well_input_filename : str
+        Name of input ".dat" well file.
+    well_locations : dict
+        Dictionary mapping well names to 2-tuple (latitude, longitude).
+    lithologies : dict
+        A dictionary mapping lithology names to pybacktrack.Lithology objects.
+    """
+    
+    print('Converting {0}...'.format(well_input_filename))
+    
+    # Start with an empty well.
+    well = pybacktrack.Well()
+    
+    # Read all lines in the input well file.
+    with open(well_input_filename, 'rU') as well_input_file:
+        lines = well_input_file.readlines()
+    
+    # Skip comments.
+    line_index = 0
+    while lines[line_index].split()[0] == 'C':
+        line_index += 1
+    
+    # Read some header data.
+    well_name = adjust_well_name(lines[line_index].strip().lower())
+    subsidence_units_in_feet = bool(float(lines[line_index+1].strip()))
+    num_layers = int(float(lines[line_index+2].strip()))
+    surface_age = float(lines[line_index+5].strip())
+    
+    # Skip more comments, but look for row starting with "C age" since has column alignment info.
+    line_index += 6
+    found_column_alignments = False
+    while lines[line_index].split()[0] == 'C':
+        comment_line = lines[line_index]
+        line_index += 1
+        if comment_line.startswith('C age'):
+            age_column_index = comment_line.find('age')
+            depth_column_index = comment_line.find('depth')
+            shale_column_index = comment_line.find('shal')
+            limestone_column_index = comment_line.find('lmst')
+            reef_column_index = comment_line.find('reef')
+            dolostone_column_index = comment_line.find('dlst')
+            sand_column_index = comment_line.find('sst')
+            basalt_column_index = comment_line.find('bslt')
+            anhydrite_column_index = comment_line.find('anhy')
+            salt_column_index = comment_line.find('salt')
+            min_water_depth_column_index = comment_line.find('PWD')
+            # Files should have PWDm and PWDM for min/max water depth but some use PWDm for both,
+            # so just search PWD for both but skip min PWD when searching for max PWD.
+            max_water_depth_column_index = comment_line.find('PWD', min_water_depth_column_index+1)
+            sea_column_index = comment_line.find('sea')
+            
+            if (age_column_index >= 0 and depth_column_index >= 0 and shale_column_index >= 0 and
+                limestone_column_index >= 0 and reef_column_index >= 0 and dolostone_column_index >= 0 and
+                sand_column_index >= 0 and basalt_column_index >= 0 and anhydrite_column_index >= 0 and
+                salt_column_index >= 0 and min_water_depth_column_index >= 0 and max_water_depth_column_index >= 0 and
+                sea_column_index >= 0):
+                found_column_alignments = True
+                # Column indices of the lithologies.
+                # The final index is the end of last lithology (beginning of min water depth). 
+                lithology_column_indices = [
+                    shale_column_index, limestone_column_index, reef_column_index,
+                    dolostone_column_index, sand_column_index, basalt_column_index,
+                    anhydrite_column_index, salt_column_index, min_water_depth_column_index]
+                    
+    if not found_column_alignments:
+        warnings.warn('Skipping well file {0}. Cannot find line starting with "C age".'.format(well_input_filename))
+        return
+    
+    # Find well location.
+    if well_name not in well_locations:
+        warnings.warn('Skipping well file {0}. Cannot find a well location with well name "{1}".'.format(
+            well_input_filename, well_name))
+        return
+    well.latitude, well.longitude = well_locations[well_name]
+    
+    # Read the stratigraphic layers.
+    top_age = surface_age
+    top_depth = 0.0
+    for layer_index in range(num_layers):
+        layer_line = lines[line_index]
+        line_index += 1
+        
+        try:
+            bottom_age = float(layer_line[age_column_index:depth_column_index])
+            bottom_depth = float(layer_line[depth_column_index:shale_column_index])
+        except ValueError:
+            warnings.warn('Skipping well file {0}. A layer is missing age or depth.'.format(well_input_filename))
+            return
+        
+        # Convert depth from feet to metres (if necessary).
+        if subsidence_units_in_feet:
+            bottom_depth *= 3.28084
+        
+        # Add the 8 hardwired lithology components.
+        total_lithology_fraction = 0.0
+        lithology_components = []
+        for lithology_index in range(0, 8):
+            lithology_fraction_string = layer_line[
+                lithology_column_indices[lithology_index]:
+                lithology_column_indices[lithology_index+1]].strip()
+            # Add lithology component unless it's just whitespace.
+            if lithology_fraction_string:
+                lithology_fraction = float(lithology_fraction_string)
+                total_lithology_fraction += lithology_fraction
+                lithology_components.append((TXT_LITHOLOGY_NAMES[lithology_index], lithology_fraction))
+        
+        # Ensure the lithology fractions add up to 1.0.
+        for lithology_component_index in range(len(lithology_components)):
+            lithology_name, lithology_fraction = lithology_components[lithology_component_index]
+            lithology_fraction /= total_lithology_fraction
+            lithology_components[lithology_component_index] = lithology_name, lithology_fraction
+        
+        try:
+            min_water_depth = float(layer_line[min_water_depth_column_index:max_water_depth_column_index])
+            max_water_depth = float(layer_line[max_water_depth_column_index:sea_column_index])
+        except ValueError:
+            warnings.warn('Skipping well file {0}. A layer is missing min or max water depth.'.format(well_input_filename))
+            return
+        # Min/max water depths get added as extra columns since they are only needed for backstripping (not backtracking).
+        other_attributes = {
+            'min_water_depth': min_water_depth,
+            'max_water_depth': max_water_depth}
+        
+        well.add_compacted_unit(
+            top_age, bottom_age,
+            top_depth, bottom_depth,
+            lithology_components, lithologies,
+            other_attributes)
+        
+        # Bottom of current layer becomes top of next layer.
+        top_age = bottom_age
+        top_depth = bottom_depth
+    
+    # Add a '_pybacktrack' suffix to the input base filename.
+    well_input_file_dir, well_input_file_basename = os.path.split(well_input_filename)
+    well_output_file_dir = os.path.join(well_input_file_dir, 'pybacktrack_format')
+    # Create output sub-directory if it doesn't exist.
+    if not os.path.isdir(well_output_file_dir):
+        os.mkdir(well_output_file_dir)
+    well_output_filename = os.path.join(well_output_file_dir, well_input_file_basename)
+    
+    # Write the well to the pyBacktrack format.
+    pybacktrack.write_well_file(
+        well,
+        well_output_filename,
+        other_column_attribute_names=['min_water_depth', 'max_water_depth'],
+        well_attributes={'longitude': 'SiteLongitude', 'latitude': 'SiteLatitude'})
+
+
 if __name__ == '__main__':
     
     # Input data files.
-    well_locations_file = 'nwshelf_wells/WELL_LOCATIONS.txt'
+    well_locations_file = 'nwshelf_wells/WELL_LOCATIONS.lst'
     well_dat_files_pattern = 'nwshelf_wells/*.dat'
+    well_txt_files_pattern = 'nwshelf_wells/*.txt'
     
     # Read the well locations.
     well_locations = read_well_locations_file(well_locations_file)
@@ -270,3 +437,8 @@ if __name__ == '__main__':
     dat_filenames = glob.glob(well_dat_files_pattern)
     for dat_filename in dat_filenames:
         convert_dat_file(dat_filename, well_locations, lithologies)
+    
+    # Read the ".txt" files - they have a slightly different format.
+    txt_filenames = glob.glob(well_txt_files_pattern)
+    for txt_filename in txt_filenames:
+        convert_txt_file(txt_filename, well_locations, lithologies)
